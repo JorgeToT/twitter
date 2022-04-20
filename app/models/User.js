17837b447bf5d3f3c13cd3886a572a3490c7e415
Jorge Aguilar <jorge.agvel@gmail.com>
2022-04-19 22:41:16 -0500

    Add more tests and more requirements for the User class

--- app/models/User.js
+++ app/models/User.js
@@ -1,12 +1,31 @@
 class User {
-    constructor(id, username, firstName, bio, dateCreated, lastUpdated) {
-        this.id = id;
-        this.username = username;
-        this.firstName = firstName;
-        this.bio = bio;
-        this.dateCreated = dateCreated;
-        this.lastUpdated = lastUpdated;
-    }
+  constructor(id, username, firstName, bio, dateCreated, lastUpdated) {
+    this.id = id;
+    this.username = username;
+    this.firstName = firstName;
+    this.bio = bio;
+    this.dateCreated = new Date();
+    this.lastUpdated = new Date();
+  }
+
+  get getUsername() {
+    return this.username;
+  }
+  get getBio() {
+    return this.bio;
+  }
+  get getDateCreated() {
+    return this.dateCreated;
+  }
+  get getLastUpdated() {
+    return this.lastUpdated;
+  }
+  set setUsername(newUsername) {
+    this.username = newUsername;
+  }
+  set setBio(newBio) {
+    this.bio = newBio;
+  }
 }
 
 module.exports = User;
--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,21 +1,34 @@
-const User = require('../../app/models/User');
+const User = require("../../app/models/User");
 
 describe("Unit tests for User Class", () => {
   test("Create an User object", () => {
-    const user = new User(
-      1,
-      "JorgeAgVel",
-      "Jorge",
-      "Bio",
-      "dateCreated",
-      "lastUpdated"
-    );
+    const user = new User(1, "JorgeAgVel", "Jorge", "Bio");
 
     expect(user.id).toBe(1);
     expect(user.username).toBe("JorgeAgVel");
     expect(user.firstName).toBe("Jorge");
     expect(user.bio).toBe("Bio");
-    expect(user.dateCreated).toBe("dateCreated");
-    expect(user.lastUpdated).toBe("lastUpdated");
+
+    expect(user.dateCreated).not.toBeUndefined(); //Verify that dateCreated is not undefined
+    expect(user.lastUpdated).not.toBeUndefined(); //Verify that lastUpdated is not undefined
+  });
+
+  test("Add getters", () => {
+    const user = new User(1, "JorgeAgVel", "Jorge", "Bio");
+
+    expect(user.getUsername).toBe("JorgeAgVel");
+    expect(user.getBio).toBe("Bio");
+    expect(user.getDateCreated).not.toBeUndefined();
+    expect(user.getLastUpdated).not.toBeUndefined();
+  });
+
+  test("Add setters", () => {
+    const user = new User(1, "JorgeAgVel", "Jorge", "Bio");
+
+    user.setUsername = "JorgeToT";
+    expect(user.username).toBe("JorgeToT");
+
+    user.setBio = "New Bio";
+    expect(user.bio).toBe("New Bio");
   });
 });
