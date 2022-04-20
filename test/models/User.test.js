b58fc47d9a2ec98190f74e2a68466851941abe6f
Jorge Aguilar <jorge.agvel@gmail.com>
2022-04-19 21:32:03 -0500

    Created test for user without import class

---
+++ test/models/User.test.js
@@ -0,0 +1,19 @@
+describe("Unit tests for User Class", () => {
+  test("Create an User object", () => {
+    const user = new User(
+      1,
+      "JorgeAgVel",
+      "Jorge",
+      "Bio",
+      "dateCreated",
+      "lastUpdated"
+    );
+
+    expect(user.id).toBe(1);
+    expect(user.username).toBe("JorgeAgVel");
+    expect(user.firstName).toBe("Jorge");
+    expect(user.bio).toBe("Bio");
+    expect(user.dateCreated).toBe("dateCreated");
+    expect(user.lastUpdated).toBe("lastUpdated");
+  });
+});
